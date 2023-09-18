13341e6cff64990a9a7e51e0cf8eb8997d16773b
Fede <federoude@gmail.com>
2023-09-18 19:58:14 -0300

    first commit

---
+++ js/products.js
@@ -0,0 +1,286 @@
+let remerasOversizeEstampadas = [
+    {
+        id: 1,
+        nombre: "Icarus",
+        tela: "Algodon",
+        colores: ["Blanco", "Lila"],
+        precio: 7,
+        talles: ["S", "M", "L", "XL", "XXL"],
+        imagen: [
+            "../assets/images/tshirts/estampadas/icarusblanca.webp",
+            "../assets/images/tshirts/estampadas/icaruslila.webp",
+            "../assets/images/tshirts/estampadas/icaruslilaback.webp"
+        ],
+        stock:true
+    },
+    {
+        id: 2,
+        nombre: "Prometeo",
+        tela: "Algodon",
+        colores: ["Blanco", "Negro"],
+        precio: 7,
+        talles: ["S", "M", "L", "XL", "XXL"],
+        imagen: [
+            "../assets/images/tshirts/estampadas/prometeoblancafrente.webp",
+            "../assets/images/tshirts/estampadas/prometeoblancaback.webp",
+            "../assets/images/tshirts/estampadas/prometeonegrafrente.webp",
+            "../assets/images/tshirts/estampadas/prometeonegraback.webp"
+        ],
+        stock:true
+    },
+    {   id: 3,
+        nombre: "Mode",
+        tela: "Algodon",
+        colores: ["Blanco", "Negro"],
+        precio: 7,
+        talles: ["S", "M", "L", "XL", "XXL"],
+        imagen: [
+            "../assets/images/tshirts/estampadas/modeblancafrente.webp",
+            "../assets/images/tshirts/estampadas/modeblancaback.webp",
+            "../assets/images/tshirts/estampadas/modenegraback.webp"
+        ],
+        stock:true
+    },
+    {
+        id: 4,
+        nombre: "Voodo",
+        tela: "Algodon",
+        colores: ["Blanco", "Negro"],
+        precio: 7,
+        talles: ["S", "M", "L", "XL", "XXL"],
+        imagen: [
+            "../assets/images/tshirts/estampadas/voodotocabacknegra",
+            "../assets/images/tshirts/estampadas/voodotocanegraback",
+            "../assets/images/tshirts/estampadas/voodotocafrenteblanca"
+        ],
+        stock:true 
+    }
+];
+
+let remerasOverLisas = [
+    {
+        id: 5,
+        nombre: "Lisa",
+        tela: "Algodon",
+        colores: ["Blanco", "Negro","Azul","Verde"],
+        precio: 7,
+        talles: ["S", "M", "L", "XL", "XXL"],
+        imagen: [
+            "../assets/images/tshirts/lisas/oversize/aszi1.webp",
+            "../assets/images/tshirts/lisas/oversize/aszi2.webp",
+            "../assets/images/tshirts/lisas/oversize/aszi3.webp",
+            "../assets/images/tshirts/lisas/oversize/aszi4.webp",
+            "../assets/images/tshirts/lisas/oversize/aszi5.webp",
+            "../assets/images/tshirts/lisas/oversize/remera-over.jpg"
+        ],
+        stock:true
+    }
+];
+
+let remerasClasicasLisas = [
+    {  id: 6,
+        nombre: "Clasica lisa",
+        tela: "Algodon",
+        colres: ["Blanco", "Negro","Azul"],
+        precio: 7,
+        talles: ["S", "M", "L", "XL", "XXL"],
+        imagen: [
+            "../assets/images/tshirts/lisas/clasics/remeazulclasica.webp",
+            "../assets/images/tshirts/lisas/clasics/remelisaclasicablanca.webp",
+            "../assets/images/tshirts/lisas/clasics/remenegraclasica.webp",
+            
+        ],
+        stock:true
+    }
+];
+
+let jeans = [
+    
+    {
+        id:7,
+        nombre: "Mom Carpintero",
+        tela: "Denim/Mezclilla",
+        colores: "Negro",
+        precio: 10,
+        talles: ["38", "40", "42", "44", "46","48","50"],
+        imagen: [
+            "../assets/images/jeans/130-jeans-mom-carpintero-.jpg",
+            "../assets/images/jeans/130-jeans-mom-carpintero-2.jpg",
+            "../assets/images/jeans/130-jeans-mom-carpintero-3.jpg",
+            "../assets/images/jeans/130-jeans-mom-carpintero-4.jpg"
+        ],
+        stock:true
+    },
+    {
+        id:8,
+        nombre: "Mom Miami",
+        tela: "Denim/Mezclilla",
+        colores: "Celeste",
+        precio: 10,
+        talles: ["38", "40", "42", "44", "46","48","50"],
+        imagen: [
+            "../assets/images/jeans/160-jeans-mom-miami.jpg",
+            "../assets/images/jeans/160-jeans-mom-miami2.jpg",
+            "../assets/images/jeans/160-jeans-mom-miami3.jpg",
+            "../assets/images/jeans/160-jeans-mom-miami4.jpg"
+        ],
+        stock:true
+    },
+    {
+        id:9,
+        nombre: "Mom Skyline",
+        tela: "Denim/Mezclilla",
+        colores: "Celeste",
+        precio: 10,
+        talles: ["38", "40", "42", "44", "46","48","50"],
+        imagen: [
+            "../assets/images/jeans/176-jean-mom-skyline.jpg",
+            "../assets/images/jeans/176-jean-mom-skyline2.jpg",
+            "../assets/images/jeans/176-jean-mom-skyline3.jpg",
+            "../assets/images/jeans/176-jean-mom-skyline4.jpg"
+        ],
+        stock:true
+    },
+    {
+        id:10,
+        nombre: "Mom Cargo",
+        tela: "Denim/Mezclilla",
+        colores: "Azul",
+        precio: 10,
+        talles: ["38", "40", "42", "44", "46","48","50"],
+        imagen: [
+            "../assets/images/jeans/220-jeans-mom-cargo.jpg",
+            "../assets/images/jeans/220-jeans-mom-cargo2.jpg",
+            "../assets/images/jeans/220-jeans-mom-cargo3.jpg",
+            "../assets/images/jeans/220-jeans-mom-cargo4.jpg"
+        ],
+        stock:true
+    },
+    {
+        id:11,
+        nombre: "Mom Chronicles",
+        tela: "Denim/Mezclilla",
+        colores: "Gris",
+        precio: 10,
+        talles: ["38", "40", "42", "44", "46","48","50"],
+        imagen: [
+            "../assets/images/jeans/242-mom-chronicles.jpg",
+            "../assets/images/jeans/242-mom-chronicles2.jpg",
+            "../assets/images/jeans/242-mom-chronicles3.jpg",
+            "../assets/images/jeans/242-mom-chronicles4.jpg"
+        ],
+        stock:true
+    }
+];
+
+let hoodies = [
+    {
+        id:12,
+        nombre: "Icarus Hood",
+        tela: "Friza",
+        colores: "Blanco",
+        precio: 10,
+        talles: ["S", "M", "L", "XL", "XXL"],
+        imagen: [
+            "../assets/images/hoodies/hoodieestampa/buzo-over-toca-icarus.webp",
+            "../assets/images/hoodies/hoodieestampa/buzo-over-toca-icarus2.webp",
+        ],
+        stock:true 
+    },
+    {
+        id:13,
+        nombre: "Smile Hood",
+        tela: "Friza",
+        colores: "Negro",
+        precio: 10,
+        talles: ["S", "M", "L", "XL", "XXL"],
+        imagen: [
+            "../assets/images/hoodies/hoodieestampa/buzo-over-smilenegro.webp",
+            "../assets/images/hoodies/hoodieestampa/buzo-over-smilenegro2.webp",
+        ],
+        stock:true
+    },
+    {
+        id:14,
+        nombre: "Liso Hood",
+        tela: "Friza",
+        colores:["Blanco", "Negro","Cakhi","Lila"],
+        precio: 10,
+        talles: ["S", "M", "L", "XL", "XXL"],
+        imagen: [
+            "../assets/images/hoodies/hoodieliso/buzo-overliso-toca-azul.webp",
+            "../assets/images/hoodies/hoodieliso/buzo-overtoca-blanco.webp",
+            "../assets/images/hoodies/hoodieliso/buzo-overtoca-lila.webp",
+            "../assets/images/hoodies/hoodieliso/buzoover-toca-caki.webp"
+        ],
+        stock:true
+    },
+    {
+        id:15,
+        nombre: "Buzo Oversize Liso",
+        tela: "Friza",
+        colores:["Blanco", "Negro","Azul","Bordo","Verde"],
+        precio: 10,
+        talles: ["S", "M", "L", "XL", "XXL"],
+        imagen: [
+            "../assets/images/hoodies/buzoover/buzo-over-rustico.jpg",
+            "../assets/images/hoodies/buzoover/Buzo-Oversize-Rustico-23-06-azul-marino-scaled.jpg",
+            "../assets/images/hoodies/buzoover/Buzo-Oversize-Rustico-23-09-blanco-scaled.jpg",
+            "../assets/images/hoodies/buzoover/Buzo-Oversize-Rustico-23-10-bordo-scaled.jpg",
+            "../assets/images/hoodies/buzoover/Buzo-Oversize-Rustico-23-39-verde-botella-scaled.jpg"
+        ],
+        stock:true
+    }
+
+];
+
+let accesorios = [
+    {
+        id:16,
+        nombre: "Mini Bag Shoulder",
+        colores:"Negro",
+        precio: 5,
+        imagen: [
+            "../assets/images/accesories/minibagshoulderblack1.webp",
+            "../assets/images/accesories/minibagshoulderblack2.webp",
+            "../assets/images/accesories/minibagshoulderblack3.webp"
+        ],
+        stock:true
+    },
+    {
+        id:17,
+        nombre: "Morral",
+        colores:"Negro",
+        precio: 5,
+        imagen: [
+            "../assets/images/accesories/morralblack1.webp",
+            "../assets/images/accesories/morralblack2.webp",
+            "../assets/images/accesories/morralblack3.webp"
+        ],
+        stock:true
+    },
+    {
+        id:18,
+        nombre: "Riñonera",
+        colores:"Negro",
+        precio: 5,
+        imagen: [
+            "../assets/images/accesories/rinoblack1.webp",
+            "../assets/images/accesories/rinoblack2.webp",
+            "../assets/images/accesories/rinoblack3.webp"
+        ],
+        stock:true
+    }
+];
+
+
+let productos = [
+    
+    remerasOversizeEstampadas,
+    remerasOverLisas,
+    remerasClasicasLisas,
+    jeans,
+    hoodies,
+    accesorios
+
+];
